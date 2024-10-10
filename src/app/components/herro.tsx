9333d425684e7eeacea6e9e38f082e85d5048edb
Muzafar Ali <[valid-email]>
2024-10-11 02:06:40 +0500

    e-commerce website using next.js/tailwind css

---
+++ src/app/components/herro.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+import Image from "next/image"
+function Herro(){
+    return(
+        <div className="w-full flex justify-center items-center">
+            <div className="sm:full md:w-full lg:w-[80%] flex justify-evenly">
+                <div className='border-r-2 border-neutral-100 p-5 sm:hidden md:hidden lg:block pb-0 '>
+                    <ul>
+                        <li className='leading-loose hover:underline cursor-pointer'>Woman's Fashion  <FontAwesomeIcon icon={faChevronRight} className='text-xs ml-2'></FontAwesomeIcon></li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Men's Fashion  <FontAwesomeIcon icon={faChevronRight} className='text-xs ml-8'></FontAwesomeIcon></li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Electronics</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Home & Lifestyle</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Medicine</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Sports & Outdoor</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Baby's & Toys</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Groceries & Pets</li>
+                        <li className='leading-loose hover:underline cursor-pointer'>Health & Beauty</li>
+                        
+                    </ul>
+                </div>
+                <div className=' sm:pt-0 md:pt-0 lg:pt-6'>
+                 <Image className='sm:w-[900px] md:w-[900px] lg:w-[700px] h-full' width={600} height={400} src={"/images/home-pic/herro.png"} alt={"herro imgae"}></Image>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Herro
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+function Contact(){
+    return(
+        <div className="w-full h-[300px] flex justify-center items-center">
+            <div className="sm:w-full md:w-[80%] flex justify-center items-center">
+                <h1 className="sm:text-lg md:text-3xl font-bold">Cantact Us Page</h1>
+            </div>
+        </div>
+    )
+}
+export default Contact
